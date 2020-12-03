1c0e7eb80c4321c2bc7ba699c4c046eb5d23945b
Stepan Samulev <stepansamulev@gmail.com>
2020-12-03 13:12:30 +0300

    Добавьте файлы проекта.

---
+++ Samulev_Homework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Samulev_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
